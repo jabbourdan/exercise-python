dee515328b4a823446a27ba7edaad6db348ac8ea
jabbour dandan <111487226+jabbourdan@users.noreply.github.com>
2022-12-04 23:10:54 +0200

    Add the colors

---
+++ 3D_tik_tok.py
@@ -0,0 +1,14 @@
+import random as rd
+
+class tic_tac_toe_3D():
+# purpose: title color and front
+    def prRed(self,skk): print("\033[91m {}\033[00m" .format(skk))
+    def prGreen(self,skk): print("\033[92m {}\033[00m" .format(skk))
+    def prPurple(self,skk): print("\033[95m {}\033[00m" .format(skk))
+    def prLightPurple(self,skk): print("\033[94m {}\033[00m" .format(skk))
+    def prCyan(self,skk): print("\033[96m {}\033[00m" .format(skk))
+    def prYellow(self,skk): print("\033[93m {}\033[00m" .format(skk))
+    def prLightGray(self,skk): print("\033[97m {}\033[00m" .format(skk))
+    def prorange(self,skk): print("\033[90m {}\033[90m" .format(skk))
+    '\033[43m'
+    def prCyan(self,skk): print("\033[96m {}\033[00m" .format(skk))
